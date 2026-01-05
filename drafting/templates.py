0fed56e15291117423bf20f5a1df98de3f662f8a
Aina Adoption Oluwasomidotun <ainaadoption@gmail.com>
2026-01-05 22:58:05 +0100

    Add pediatric and OBGYN templates

---
+++ drafting/templates.py
@@ -0,0 +1,18 @@
+PEDIATRIC_TEMPLATE = """
+Bio Data:
+Age: {age}
+Sex: {sex}
+
+Presenting Complaints:
+{complaints}
+"""
+
+OBGYN_TEMPLATE = """
+Bio Data:
+Age: {age}
+Gravida: {gravida}
+Para: {para}
+
+Presenting Complaints:
+{complaints}
+"""
